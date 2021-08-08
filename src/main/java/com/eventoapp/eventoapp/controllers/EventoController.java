e97c6c14ddb5aaa31ed7459aac6b1d856e0fe052
acferlucas <lafs@discente.ifpe.edu.br>
2021-08-07 22:59:31 -0300

    create materialize css

--- src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventoapp.eventoapp.models.Evento;
 import com.eventoapp.eventoapp.repository.EventoRepository;
@@ -25,5 +26,15 @@ public class EventoController {
 		er.save(evento); 
 		
 		 return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = er.findAll();
+		
+		mv.addObject("eventos",eventos);
+		return mv;
 	}
 }
